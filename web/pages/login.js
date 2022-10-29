231523aa66513b2053ac6001a858d2366d27b6a9
Afonso Silva <afonso.silva@bringglobal.com>
2022-10-29 22:57:48 +0100

    add: styles homepage

--- web/pages/login.js
+++ web/pages/login.js
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import LoginButton from '../components/LoginButton'
@@ -9,8 +8,8 @@ export default function Login() {
             <div className="h-screen flex flex-col">
                 <div className="basis-1/12"></div>
                 <div className="basis-6/12 pt-5">
-                    <Image width={450} height={150} style={{ margin: "auto", maxWidth: "350px", marginBottom: '50px'}} alt='' src={'/images/logo-red-450x150.png'} />
-                    <Image width={450} height={450} style={{ margin: "auto", marginBottom: '40px'}} alt='' src={'/images/finance.jpg'} />
+                    <Image width={400} height={150} style={{ margin: "auto", maxWidth: "300px", marginBottom: '50px'}} alt='' src={'/images/logo-red-450x150.png'} />
+                    <Image width={400} height={450} style={{ margin: "auto", marginBottom: '40px'}} alt='' src={'/images/finance.jpg'} />
                 </div>
 
                 <div className="basis-2/12 pb-5">
@@ -19,7 +18,8 @@ export default function Login() {
 
                 <div className="basis-2/12">
                     <div className="btn-grey hover:btn-red-hover w-4/5">
-                        Create an account (COMING SOON)
+                        Create an account <sup><span style={{fontSize: '10px'}}>SOON</span></sup>
+                        
                     </div>
                 </div>
                 <div className="basis-1/12 pb-5"></div>
