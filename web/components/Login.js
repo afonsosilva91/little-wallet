d22e397f28c06089f95731017ee04682e23b69e5
Ruben Dinis <ruben.sousa.dinis@gmail.com>
2022-10-29 16:06:29 +0100

    Add cronos and gnosis testnet connections

--- web/components/Login.js
+++ web/components/Login.js
@@ -1,12 +1,24 @@
 import { Web3Modal, Web3Button, useAccount, useDisconnect } from '@web3modal/react'
+import { providers } from '@web3modal/ethereum'
+import { gnosisTestnet, cronosTestnet } from "../utils/networks"
 
 const config = {
   projectId: '8f6b3f536190073bbe77b9d66f4d22da',
   theme: 'light',
-  accentColor: 'default',
+  accentColor: "default",
   ethereum: {
-    appName: 'Little Wallet'
-  }
+    appName: 'Little Wallet',
+    autoConnect: true,
+    chains: [
+      gnosisTestnet,
+      cronosTestnet
+    ],
+    providers: [
+      providers.walletConnectProvider({
+        projectId: "8f6b3f536190073bbe77b9d66f4d22da",
+      }),
+    ],
+  },
 }
 
 export default function Login() {
